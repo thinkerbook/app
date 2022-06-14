029e476ee20f15254cd81c0f3007d7af674f344b
dev-thinkerbook <dev@thinkerbook.info>
2022-06-14 19:15:54 +0200

    extensible infocard, remove dismissible

--- src/components/InfoCard.vue
+++ src/components/InfoCard.vue
@@ -1,15 +1,9 @@
 <template>
-  <div v-if="!dismissible || getConfig().showInfoOnStart" class="card border-0">
-    <div class="card-body alert alert-light alert-dismissible fade show" style="margin-bottom: 0">
-      <button
-        v-if="dismissible"
-        type="button"
-        class="btn-close"
-        data-bs-dismiss="alert"
-        aria-label="Close"
-        @click="dismissInfo"
-      ></button>
-
+  <div class="card border-0">
+    <div
+      class="card-body alert alert-light"
+      style="margin-bottom: 0"
+    >
       <p class="card-text" style="text-align: justify">
         Tous les livres recommandés dans les interviews de la chaine
         <a href="https://www.thinkerview.com/" target="_blank">ThinkerView</a>
@@ -19,61 +13,49 @@
           </a>)
         </span>
 
+        <span v-if="isShowAll">
+          ainsi que le conseil aux jeunes générations (une bouteille à la mer)
+        </span>
+        .
+
         <span
           v-if="!isShowAll"
           class="btn btn-outline-secondary btn-sm-xx ms-2"
           title="Afficher plus"
-          @click="doShowAll"
+          @click="doDisplayAll(true)"
         >
-          ...
-        </span>
-
-        <span v-if="isShowAll">
-          ainsi que le conseil aux jeunes générations (une bouteille à la mer).
+          v
         </span>
       </p>
+
       <p v-if="isShowAll" class="card-text" style="text-align: justify; font-size: smaller">
         <i class="fas fa-thumbs-up"></i>
         Tips: pour accéder directement à ThinkerBook depuis une vidéo Youtube, ajouter <code>-tbk</code> dans l'URL :
         <i>https://www.youtube<code>-tbk</code>.com/watch?v=6VUpicNBMzg</i>
+
+        <span
+          v-if="isShowAll"
+          class="btn btn-outline-secondary btn-sm-xx ms-2"
+          title="Afficher moins"
+          @click="doDisplayAll(false)"
+        >
+          ^
+        </span>
       </p>
     </div>
   </div>
 </template>
 
 <script>
-import ls from "localstorage-slim";
-// dismiss for a month
-const DISMISS_TIMEOUT = 60 * 60 * 24 * 30;
-
 export default {
   props: {
-    dismissible: {
-      type: Boolean,
-      default: false,
-    },
     showAll: {
       type: Boolean,
       default: true,
     },
   },
   setup() {
-    function getConfig() {
-      const defaultConfig = {
-        showInfoOnStart: true,
-      };
-      return ls.get("config") || defaultConfig;
-    }
-
-    function setConfig(config) {
-      const value = Object.assign({}, config);
-      ls.set("config", value, { ttl: DISMISS_TIMEOUT });
-    }
-
-    return {
-      getConfig,
-      setConfig,
-    };
+    return {};
   },
   data() {
     return {
@@ -81,12 +63,8 @@ export default {
     };
   },
   methods: {
-    dismissInfo() {
-      console.log("dismissInfo");
-      this.setConfig({ showInfoOnStart: false });
-    },
-    doShowAll() {
-      this.isShowAll = true;
+    doDisplayAll(all) {
+      this.isShowAll = all;
     },
   },
 };
